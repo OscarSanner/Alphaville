e7b0566615dfc0ca14bccb12ba318fca025f448b
bomandavid <david.boman99@hotmail.com>
2022-04-25 15:59:47 +0200

    skapat searchbar, dock i main activity, borde kanske vara någon annanstans

--- app/src/main/java/com/alphaville/coffeeapplication/MainActivity.java
+++ app/src/main/java/com/alphaville/coffeeapplication/MainActivity.java
@@ -3,12 +3,50 @@ package com.alphaville.coffeeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] name = {"A", "B", "C"}; // tillfälligt
+
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.lv_listView);
+        // byt ut name mot kort för kaffe
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.nav_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                arrayAdapter.getFilter().filter(s);
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ app/src/main/java/com/alphaville/coffeeapplication/SearchFragment.java
@@ -0,0 +1,8 @@
+package com.alphaville.coffeeapplication;
+
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+
+
+}
