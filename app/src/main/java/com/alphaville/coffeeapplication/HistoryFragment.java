d8d52dbce929896711d5256b96b9a8f12b2bd782
EdvinBrogeland <ebrogeland@gmail.com>
2022-04-25 14:55:51 +0200

    Added Button functionality
    Added fragments for history, recommendations, search. Made the buttons in navbar send user to the different fragments.

--- app/src/main/java/com/alphaville/coffeeapplication/HistoryFragment.java
+++ app/src/main/java/com/alphaville/coffeeapplication/HistoryFragment.java
@@ -9,10 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class RecommendationsFragment extends Fragment {
+public class HistoryFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recommendations,container,false);
+        return inflater.inflate(R.layout.fragment_history,container,false);
     }
 }
