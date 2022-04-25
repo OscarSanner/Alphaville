7929bda47e32fa124d96837e13b68f3e867ca0cd
Valle20 <valdemar433@gmail.com>
2022-04-25 21:58:42 +0200

    Small changes

--- app/src/main/java/com/alphaville/coffeeapplication/CoffeeInfoViewFragment.java
+++ app/src/main/java/com/alphaville/coffeeapplication/CoffeeInfoViewFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CoffeInfoViewFragment#newInstance} factory method to
+ * Use the {@link CoffeeInfoViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CoffeInfoViewFragment extends Fragment {
+public class CoffeeInfoViewFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class CoffeInfoViewFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CoffeInfoViewFragment() {
+    public CoffeeInfoViewFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class CoffeInfoViewFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CoffeInfoViewFragment.
+     * @return A new instance of fragment CoffeeInfoViewFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CoffeInfoViewFragment newInstance(String param1, String param2) {
-        CoffeInfoViewFragment fragment = new CoffeInfoViewFragment();
+    public static CoffeeInfoViewFragment newInstance(String param1, String param2) {
+        CoffeeInfoViewFragment fragment = new CoffeeInfoViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
