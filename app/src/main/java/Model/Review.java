32bb4044e0903a2cc42c858b96e1b5f8dd28dc76
felixedholm <felixedholm@gmail.com>
2022-04-25 15:56:51 +0200

    Added text attribute and getter + setter

--- app/src/main/java/Model/Review.java
+++ app/src/main/java/Model/Review.java
@@ -1,4 +1,21 @@
 package Model;
 
 public class Review {
+
+    //CoffeeProduct coffeeProduct;
+    private String textReview;
+    //TODO Add more attributes
+
+
+    public Review(String textReview) {
+        this.textReview = textReview;
+    }
+
+    public String getTextReview() {
+        return textReview;
+    }
+
+    public void setTextReview(String textReview) {
+        this.textReview = textReview;
+    }
 }
