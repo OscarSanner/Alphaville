dce705b10c9533f2c5135c22c19586ab0a117858
felixedholm <felixedholm@gmail.com>
2022-04-25 16:47:11 +0200

    Added onCreateView method to ReviewDataFragment and enabled databinding

--- app/src/main/java/View/ReviewDataFragment.java
+++ app/src/main/java/View/ReviewDataFragment.java
@@ -1,6 +1,28 @@
 package View;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.alphaville.coffeeapplication.R;
+import com.alphaville.coffeeapplication.databinding.ReviewDataFragmentBinding;
+
+
 public class ReviewDataFragment extends Fragment {
+
+    ReviewDataFragmentBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.review_data_fragment, container, false);
+
+    return binding.getRoot();
+    }
 }
