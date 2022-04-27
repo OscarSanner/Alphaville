c545899817fbf1a4d89eefc665e0bb01ea34b173
Valle20 <valdemar433@gmail.com>
2022-04-27 10:24:28 +0200

    More methods for changing textfields

--- app/src/main/java/com/alphaville/coffeeapplication/CoffeeInfoViewFragment.java
+++ app/src/main/java/com/alphaville/coffeeapplication/CoffeeInfoViewFragment.java
@@ -1,13 +1,17 @@
 package com.alphaville.coffeeapplication;
 
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.alphaville.coffeeapplication.databinding.CoffeeInfoViewFragmentBinding;
 
@@ -21,11 +25,15 @@ public class CoffeeInfoViewFragment extends Fragment {
     public View onCreateView (LayoutInflater inflater,
                           ViewGroup container,
                           Bundle savedInstanceState) {
-    binding = CoffeeInfoViewFragmentBinding.inflate(inflater, container, false);
-    View view = binding.getRoot();
-    return view;
+        binding = CoffeeInfoViewFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+        /*
+        setCoffeeAttributes();
+        setCoffeeInformation();
+        setCoffeePicture();
 
-        /*//listener for the review button
+        //listener for the review button
         binding.reviewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +47,31 @@ public class CoffeeInfoViewFragment extends Fragment {
             public void onClick(View v) {
                 //viewmodel.changeLikeStatus(binding.likeBtn.isChecked());
             }
-        });*/
+        });
+        */
     }
+
+    private void setCoffeeInformation(String name, String info, String description){
+        binding.coffeenameText.setText(name);
+        binding.infoText.setText(info);
+        binding.descriptionText.setText(description);
+    }
+
+    private void setCoffeeAttributes(String hight, String flavour, String country, String region,
+                                     String process, String rostery, String brand){
+        binding.hightText.setText(hight);
+        binding.flavourText.setText(flavour);
+        binding.countryText.setText(country);
+        binding.regionText.setText(region);
+        binding.processText.setText(process);
+        binding.rosteryText.setText(rostery);
+        binding.brandText.setText(brand);
+    }
+
+    //Don't know which setImage-method to use here
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setCoffeePicture(Icon image){
+        binding.coffeepicture.setImageIcon(image);
+    }
+
 }
