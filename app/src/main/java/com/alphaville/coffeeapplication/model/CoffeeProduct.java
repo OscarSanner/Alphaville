d2b44322d5b82052fb10d6f65439438315109500
dubbell <46517201+dubbell@users.noreply.github.com>
2022-04-27 20:54:32 +0200

    Updated attributes in CoffeeProduct.java

--- app/src/main/java/com/alphaville/coffeeapplication/model/CoffeeProduct.java
+++ app/src/main/java/com/alphaville/coffeeapplication/model/CoffeeProduct.java
@@ -11,19 +11,21 @@ import java.util.List;
 public class CoffeeProduct
 {
     /** Possible ways for a coffee product to be processed. */
-    public enum Process { dry, wet, honey, anaerobic, carbonic_maceration, giling_basah }
+    public enum Process { dry, wet, fermented, honey }
     /** Possible tastes for a coffee product. */
     public enum Taste { floral, fruity, sour_fermented, green_vegetative, roasted, spices, nutty_cocoa, sweet, other }
+    /** Possible roasts for each coffee product. */
+    public enum Roast { light, dark }
 
     /** The name of the coffee product */
     private final String name;
-    /** The name of the roastery from which the product originates. */
-    private final String roastery;
     /** The name of the country from which the product originates. */
     private final String country;
 
     /** Elevation above sea level of the product's roastery. */
     private final int elevation;
+    /** The method by which the coffee bean is roasted. */
+    private final Roast roast;
     /** The way this product has been processed. */
     private final Process process;
     /** A list of all taste profiles that this product matches. */
@@ -32,12 +34,12 @@ public class CoffeeProduct
     /** Short description of the product. */
     private final String description;
 
-    /** Whether the user likes the coffeeproduct. */
+    /** Whether the user likes the coffee product. */
     private final boolean isLiked;
 
-    public CoffeeProduct(String name, String roastery, String country, int elevation, Process process, List<Taste> tastes, String description, boolean isLiked){
+    public CoffeeProduct(String name, String country, int elevation, Roast roast, Process process, List<Taste> tastes, String description, boolean isLiked){
         this.name = name;
-        this.roastery = roastery;
+        this.roast = roast;
         this. country = country;
 
         this.elevation = elevation;
@@ -52,11 +54,11 @@ public class CoffeeProduct
 
     public String getName() { return name; }
 
-    public String getRoastery() { return roastery; }
-
     public String getCountry() { return country; }
 
     public int getElevation() { return elevation; }
+
+    public Roast getRoast() { return roast; }
 
     public Process getProcess() { return process; }
 
