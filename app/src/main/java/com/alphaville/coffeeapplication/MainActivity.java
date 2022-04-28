edd54d79c36af0dffecf33bac91e5e393de5cf7d
EdvinBrogeland <ebrogeland@gmail.com>
2022-04-28 13:41:33 +0200

    Reverted accidental changes in activity_main

--- app/src/main/java/com/alphaville/coffeeapplication/MainActivity.java
+++ app/src/main/java/com/alphaville/coffeeapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.review_data_fragment);
+        setContentView(R.layout.activity_main);
 
     }
 }
